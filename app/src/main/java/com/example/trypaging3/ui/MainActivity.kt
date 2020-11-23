25ec1de9540b0100b2f274485c4ca70b3a9dd9df
Renaldy <renaldy@Renaldys-MacBook-Pro.local>
2020-11-23 11:15:13 +0700

    [add] adding load and button retry in main activity

--- app/src/main/java/com/example/trypaging3/ui/MainActivity.kt
+++ app/src/main/java/com/example/trypaging3/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.trypaging3.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trypaging3.Injection
@@ -32,10 +35,28 @@ class MainActivity : AppCompatActivity() {
             .get(MainViewModel::class.java)
 
         getDataGame()
+        initAdapter()
 
+        retry_button_main.setOnClickListener { gameAdapter.retry() }
+    }
+
+    private fun initAdapter(){
         rv_game.adapter = gameAdapter.withLoadStateFooter(
             footer = GameLoadStateAdapter { gameAdapter.retry() }
         )
+        gameAdapter.addLoadStateListener { loadState ->
+            rv_game.isVisible = loadState.source.refresh is LoadState.NotLoading
+            pb_main.isVisible = loadState.source.refresh is LoadState.Loading
+            retry_button_main.isVisible = loadState.source.refresh is LoadState.Error
+
+            val errorState = loadState.source.append as? LoadState.Error
+                ?: loadState.source.prepend as? LoadState.Error
+                ?: loadState.append as? LoadState.Error
+                ?: loadState.prepend as? LoadState.Error
+            errorState?.let {
+                Toast.makeText(this, "error ${it.error}", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     private fun getDataGame(){
